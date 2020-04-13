b926a82ecdb1ce764e0bff5626b0d081e772d594
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-04-13 22:25:11 +0100

    5 implement Login

--- 1.Securing-React-Apps-with-AuthO/src/App.jsx
+++ 1.Securing-React-Apps-with-AuthO/src/App.jsx
@@ -1,21 +1,43 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route } from 'react-router-dom';
 import Home from './views/Home';
 import Profile from './views/Profile';
 import Nav from './components/Nav';
+import Callback from './views/Callback';
+import Auth from './Auth/Auth';
 
-function App() {
-  return (
-    <Fragment>
-      <Nav />
-      <div className="body">
-        <Route path="/" exact component={Home}></Route>
-        <Route path="/profile" exact component={Profile}></Route>
-      </div>
-    </Fragment>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    //isnt necessary put on stage
+    this.auth = new Auth(this.props.history);
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Nav />
+        <div className="body">
+          <Route
+            exact
+            path="/"
+            exact
+            render={(props) => <Home auth={this.auth} {...props} />}
+          ></Route>
+
+          <Route
+            exact
+            path="/callback"
+            exact
+            render={(props) => <Callback auth={this.auth} {...props} />}
+          ></Route>
+          
+          <Route path="/profile" exact component={Profile}></Route>
+        </div>
+      </Fragment>
+    );
+  }
 }
-
-export default App;
--- 1.Securing-React-Apps-with-AuthO/src/views/Home.jsx
+++ 1.Securing-React-Apps-with-AuthO/src/views/Home.jsx
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
+    const { isAuthenticated, login} = this.props.auth;
+
+    console.log(this.props.auth)
     return (
       <div>
         <h1>Home</h1>
+        {isAuthenticated() ? (
+          <Link to="/profile">View Profile</Link>
+        ) : (
+          <button onClick={login}>Log In</button>
+        )}
       </div>
-    )
+    );
   }
 }

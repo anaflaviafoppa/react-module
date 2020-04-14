bc9081c8745f092d8f4734f932909bba5ad92d71
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-04-14 16:12:30 +0100

    7 api authorization fundamentals working

--- 1.Securing-React-Apps-with-AuthO/server.js
+++ 1.Securing-React-Apps-with-AuthO/server.js
@@ -1,41 +1,55 @@
-const express = require("express");
-require("dotenv").config();
-const jwt = require("express-jwt"); // Validate JWT and set req.user
-const jwksRsa = require("jwks-rsa"); // Retrieve RSA keys from a JSON Web Key set (JWKS) endpoint
+const express = require('express');
+require('dotenv').config();
+const jwt = require('express-jwt'); // Validate JWT and set req.user
+//const jwksRsa = require('jwks-rsa'); // Retrieve RSA keys from a JSON Web Key set (JWKS) endpoint
 
-const checkJwt = jwt({
-  // Dynamically provide a signing key based on the kid in the header
-  // and the signing keys provided by the JWKS endpoint.
-  secret: jwksRsa.expressJwtSecret({
-    cache: true, // cache the signing key
+var jwks = require('jwks-rsa');
+
+// const checkJwt = jwt({
+//   // Dynamically provide a signing key based on the kid in the header
+//   // and the signing keys provided by the JWKS endpoint.
+//   secret: jwksRsa.expressJwtSecret({
+//     cache: true, // cache the signing key
+//     rateLimit: true,
+//     jwksRequestsPerMinute: 5, // prevent attackers from requesting more than 5 per minute
+//     jwksUri: `https://${
+//       process.env.REACT_APP_AUTH0_DOMAIN
+//     }/.well-known/jwks.json`
+//   }),
+
+//   // Validate the audience and the issuer.
+//   audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+//   issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
+
+//   // This must match the algorithm selected in the Auth0 dashboard under your app's advanced settings under the OAuth tab
+//   algorithms: ['RS256']
+// });
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
     rateLimit: true,
-    jwksRequestsPerMinute: 5, // prevent attackers from requesting more than 5 per minute
-    jwksUri: `https://${
-      process.env.REACT_APP_AUTH0_DOMAIN
-    }/.well-known/jwks.json`
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://dev-anaflaviafoppa.eu.auth0.com/.well-known/jwks.json',
   }),
-
-  // Validate the audience and the issuer.
-  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
-  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
-
-  // This must match the algorithm selected in the Auth0 dashboard under your app's advanced settings under the OAuth tab
-  algorithms: ["RS256"]
+  audience: 'http://localhost:5000',
+  issuer: 'https://dev-anaflaviafoppa.eu.auth0.com/',
+  algorithms: ['RS256'],
 });
 
 const app = express();
 
-app.get("/public", function(req, res) {
+app.get('/public', function (req, res) {
   res.json({
-    message: "Hello from a public API!"
+    message: 'Hello from a public API!',
   });
 });
 
-app.get("/private", checkJwt, function(req, res) {
+app.get('/private', jwtCheck, function (req, res) {
   res.json({
-    message: "Hello from a private API!"
+    message: 'Hello from a private API!',
   });
 });
 
-app.listen(3001);
-console.log("API server listening on " + process.env.REACT_APP_AUTH0_AUDIENCE);
+app.listen(5000);
+console.log('API server listening on ' + process.env.REACT_APP_AUTH0_AUDIENCE);

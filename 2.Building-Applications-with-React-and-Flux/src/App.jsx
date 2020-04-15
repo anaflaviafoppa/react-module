3de8338eda3d197f0b6fc8b7cc69d7112545b6ca
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-04-15 21:05:45 +0100

    Creating React Components

--- 2.Building-Applications-with-React-and-Flux/src/App.jsx
+++ 2.Building-Applications-with-React-and-Flux/src/App.jsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+import Header from './components/common/Header';
 
-function App() {
+const App = () => {
+  function getPage() {
+    const route = window.location.pathname;
+    if (route === '/about') return <AboutPage />;
+    return <HomePage />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <Header />
+      {getPage()};
     </div>
   );
-}
+};
 
 export default App;
---
+++ 2.Building-Applications-with-React-and-Flux/src/components/AboutPage/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react';
+
+export default class AboutPage extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h2>About</h2>
+        <p>This app uses React.</p>
+      </Fragment>
+    );
+  }
+}

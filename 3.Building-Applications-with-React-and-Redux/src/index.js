5de3c4b66ab16a389b943f4ddbf9892bc25d97c7
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-04-22 21:43:54 +0100

    9.13 Instantiate Store and Provider

--- 3.Building-Applications-with-React-and-Redux/src/index.js
+++ 3.Building-Applications-with-React-and-Redux/src/index.js
@@ -1,13 +1,20 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react';
+import { render } from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
-import "./style.css";
+import './style.css';
+import configureStore from './redux/configureStore';
+//This will provide Redux store data to our React components:
+import { Provider as ReduxProvider } from 'react-redux';
+
+const store = configureStore();
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById('app')
 );

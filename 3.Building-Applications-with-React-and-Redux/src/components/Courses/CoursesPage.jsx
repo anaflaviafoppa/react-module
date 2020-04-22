a1c4e018b84d4b2467850b96362865924f7015a3
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-04-22 19:40:49 +0100

    9.3 Binding in Classes

--- 3.Building-Applications-with-React-and-Redux/src/components/Courses/CoursesPage.jsx
+++ 3.Building-Applications-with-React-and-Redux/src/components/Courses/CoursesPage.jsx
@@ -28,9 +28,10 @@ export default class CoursesPage extends Component {
         title: '',
       },
     };
+    
   }
 
-  handleChange(event) {
+  handleChange = (event) => {
     const course = { ...this.state.course, title: event.target.value };
     this.setState({ course });
   }
@@ -40,7 +41,7 @@ export default class CoursesPage extends Component {
       <Fragment>
         <h1>Courses</h1>
         <form>
-          <h3>Ad Course</h3>
+          <h3>Add Course</h3>
           <input type="text" onChange={this.handleChange} value={this.state.course.title} />
           <input type="submit" value="Save" />
         </form>

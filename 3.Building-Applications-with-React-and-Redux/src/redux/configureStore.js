1c053d12c66e654091e230115334a955d4ddf0e6
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-04-22 21:38:08 +0100

    9.12 Configure Store

---
+++ 3.Building-Applications-with-React-and-Redux/src/redux/configureStore.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducers/courseReducer';
+
+//Evita de mutate o state
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+
+
+
+export default function configureSotre(initialState) {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  return createStore(
+    rootReducer, 
+    initialState, 
+    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+    );
+}

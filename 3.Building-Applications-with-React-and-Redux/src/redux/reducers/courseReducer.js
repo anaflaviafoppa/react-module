3e7fa5f5e732b9dced080eea5f918f2a663fe446
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-04-22 21:26:34 +0100

    9.11 First Reducer

---
+++ 3.Building-Applications-with-React-and-Redux/src/redux/reducers/courseReducer.js
@@ -0,0 +1,10 @@
+export default function courseReducer(state = [], action) {
+  switch (action.type) {
+    case 'CREATE_COURSE':
+      //This is will mutate the state:
+      //state.push(action.course)
+      return [...state, { ...action.course }];
+    default:
+      return state;
+  }
+}

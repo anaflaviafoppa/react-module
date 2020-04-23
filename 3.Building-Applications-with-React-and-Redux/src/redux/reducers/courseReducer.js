eb6d4ea9dee81506b1b1b908171b2d35833b2d48
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-04-23 16:20:22 +0100

    9.18 Actions type

--- 3.Building-Applications-with-React-and-Redux/src/redux/reducers/courseReducer.js
+++ 3.Building-Applications-with-React-and-Redux/src/redux/reducers/courseReducer.js
@@ -1,7 +1,7 @@
+import * as types from '../actions/actionTypes';
 export default function courseReducer(state = [], action) {
   switch (action.type) {
-    case 'CREATE_COURSE':
-    
+    case types.CREATE_COURSE:
       //This is will mutate the state:
       //state.push(action.course)
       return [...state, { ...action.course }];

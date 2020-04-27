fca03f667aebef294c47e5c2abd06d97d25ec1cf
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-04-27 11:36:01 +0100

    11.4 Router for ManageCoursePage

--- 3.Building-Applications-with-React-and-Redux/src/App.jsx
+++ 3.Building-Applications-with-React-and-Redux/src/App.jsx
@@ -5,7 +5,7 @@ import AboutPage from './components/About/AboutPage.jsx';
 import Header from './components/common/Header.jsx';
 import NotFoundPage from './components/PagenotFound/NotFoundPage.jsx';
 import CoursesPage from './components/Courses/CoursesPage.jsx';
-
+import ManegeCoursePage from './components/Courses/ManageCoursePage.jsx';
 function App() {
   return (
     <div className="container-fluid">
@@ -14,10 +14,10 @@ function App() {
         <Route path="/" exact component={HomePage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/courses" component={CoursesPage} />
-        {/* <Route path="/course/:slug" component={ManegeCoursePage} />
+        <Route path="/course/:slug" component={ManegeCoursePage} />
         <Route path="/course" component={ManegeCoursePage} />
 
-        <Redirect from="/about-page" to="about" /> */}
+        {/*<Redirect from="/about-page" to="about" /> */}
         <Route component={NotFoundPage} />
       </Switch>
     </div>
